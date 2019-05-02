e4109751e9e25fc44242c107c8c99dd7d939b368
SPetrenko17 <SPetrenko17@yandex.ru>
2019-05-02 20:20:34 +0300

    Initial Commit

---
+++ BMSTudent/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMSTudent
+//
+//  Created by Sergei Petrenko on 02/05/2019.
+//  Copyright © 2019 Sergei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
